533cce049e574d7ceb9f7c912d6039190a28cebd
Eugen Sawin <esawin@me73.com>
2013-01-21 14:00:32 +0100

    Added ontology index skeletons.

---
+++ src/nlp/ontology-index.cc
@@ -0,0 +1,8 @@
+// Copyright 2013 Eugen Sawin <esawin@me73.com>
+#include "./ontology-index.h"
+
+namespace pyt {
+namespace nlp {
+
+}  // namespace nlp
+}  // namespace pyt
---
+++ src/nlp/ontology-index.h
@@ -0,0 +1,15 @@
+// Copyright 2013 Eugen Sawin <esawin@me73.com>
+#ifndef SRC_NLP_ONTOLOGY_INDEX_H_
+#define SRC_NLP_ONTOLOGY_INDEX_H_
+
+namespace pyt {
+namespace nlp {
+
+class OntologyIndex {
+ public:
+
+};
+
+}  // namespace nlp
+}  // namespace pyt
+#endif  // SRC_NLP_ONTOLOGY_INDEX_H_
